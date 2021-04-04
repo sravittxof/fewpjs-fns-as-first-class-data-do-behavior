8f3e8c8fc2a300db0c6158a21081fb455fb8e271
sravittxof <travis.fox90@gmail.come>
2021-04-03 23:52:41 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const timeSplit = time.split(':');
+  const timeInt = parseInt(timeSplit[0], 10);
+
+  if (timeInt < 12) {
+    return "Good Morning";
+  } else if (timeInt < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  const currentMessage = document.getElementById('greeting');
+  currentMessage.innerText = message;
+}
